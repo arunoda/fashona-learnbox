24bc4cc5bfae2140d0cdb6c8fb0119a06787cf93
soori <charithsoori@gmail.com>
2011-09-20 20:21:55 +0530

    working without ranking

---
+++ ass1/requestHandler.js
@@ -0,0 +1,135 @@
+var http = require("http");
+var arrtweetedUsers =[];
+function start(req, res){
+	query = req.url.split("?")[1];
+	res.send('Hello World start');
+	console.log(query);
+
+	//var arrtweetedUsers =[];
+	getTweets(query);
+	//get_tweets(query);
+	setInterval(getTweets, 10000);  
+}
+
+function view(req, res){
+	res.send('Hello World veiw');
+}
+
+exports.start	= start;
+exports.view	=view;
+
+
+
+
+function getTweets(query){
+	console.log("getTweets called");
+	 // Send a search request to Twitter
+	var request = http.request({
+		
+		host: "search.twitter.com",
+	    port: 80,
+	    method: "GET",
+    	path: "/search.json?q=" + query
+	})
+	console.log("http req "+ request);
+	
+	request.addListener("response", function(response) {  
+		console.log("inside request.addListener");
+        var body = "";  
+        response.addListener("data", function(data) {
+        	console.log("inside response.addListener");
+            body += data;
+           // console.log(body);
+        });  
+  
+        response.addListener("end", function() {  
+        	console.log("inside response.addListener end");
+            var tweets = JSON.parse(body);
+           // console.log(tweets.results.length);
+            //console.log(tweets.results[1].from_user);
+            //console.log(tweets.from_user);
+           // console.log(tweets.length()); 
+            if(tweets.results.length > 0) {  
+                //tweet_emitter.emit("tweets", tweets); 
+                console.log(tweets.results.length);
+           		console.log(tweets.results[1].from_user);
+           		tweetedUsers(tweets);
+            }  
+        });  
+    });  
+  
+    request.end(); 
+}
+
+
+
+
+function tweetedUsers(tweets){
+	//var tweetedUsers =[];
+	console.log("in function tweetedUsers");
+	for(var i=1; i< tweets.results.length; i++){
+		//console.log(tweets.results[i].from_user);
+		arrtweetedUsers.push(tweets.results[i].from_user);
+	}
+		
+		
+	console.log(arrtweetedUsers.length+" "+arrtweetedUsers);
+	
+}
+
+/*
+function rankUsers(){
+	var rank=[];
+	for(var i=1; i< tweets.results.length; i++){
+		rank.push()
+	}
+}*/
+
+
+
+
+
+
+
+
+
+
+
+//search in twitter api
+ function get_tweets(query) {
+  
+  // Send a search request to Twitter
+  var request = http.request({
+    host: "search.twitter.com",
+    port: 80,
+    method: "GET",
+    path: "/search.json?q=" + query
+  })
+  
+  //on response execute 
+ .on("response", function(response){
+ 	var body = "";
+    
+	response.on("data", function(data){
+    body += data;
+      
+    try {
+	    var tweets = JSON.parse(body);
+	    console.log(tweets);
+        
+        if (tweets.results.length > 0) {
+
+          //boroads cast tweets
+          Twitter.EventEmitter.emit("tweets", tweets);
+          console.log(tweets);
+        }
+        
+        Twitter.EventEmitter.removeAllListeners("tweets");
+      } 
+      catch (ex) {
+       console.log("waiting for more data chunks...");
+      }
+    });
+  });
+  request.end();
+ }
---
+++ ass1/server.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var requestHandler	= require("./requestHandler");
+
+
+var server 	= express.createServer();
+
+server.get('/',function(req, res){
+	 requestHandler.start(req, res);
+});
+
+server.get('/view',function(req, res){
+	requestHandler.view(req, res);
+});
+
+server.listen(8000);
+console.log('Express server started on port %s', server.address().port);
+
+exports.server=server;
+
+
+
+
+
+
+
+/*
+function start(){
+	console.log("testing 123..............");
+ 
+}
+
+function view(){
+	
+}*/
