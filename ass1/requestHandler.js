c70e6fa30f7d76645d46aa10858ae3e5b6bf62e4
soori <charithsoori@gmail.com>
2011-09-22 15:36:04 +0530

    recoverd lost request handler

--- ass1/requestHandler.js
+++ ass1/requestHandler.js
@@ -1,87 +1,87 @@
 var http = require("http");
 var arrtweetedUsers =[];
 exports.start=function start(req, res){
-	
-	query = req.url.split("?")[1];
-	res.send('Hello World start');
-	console.log(query);
+
+query = req.url.split("?")[1];
+res.send('Hello World start');
+console.log(query);
 
 
-	getTweets(query);
-	
-	setInterval(getTweets, 10000);  
+getTweets(query);
+
+setInterval(getTweets, 10000);
 }
 
 function view(req, res){
-	res.send('Hello World veiw');
+res.send('Hello World veiw');
 }
 
-//exports.start	= start;
-exports.view	=view;
+//exports.start = start;
+exports.view =view;
 
 
 
 
 function getTweets(query){
-	console.log("getTweets called");
-	 // Send a search request to Twitter
-	var request = http.request({
-		
-		host: "search.twitter.com",
-	    port: 80,
-	    method: "GET",
-    	path: "/search.json?q=" + query
-	})
-	console.log("http req "+ request);
-	
-	request.addListener("response", function(response) {  
-		console.log("inside request.addListener");
-        var body = "";  
+console.log("getTweets called");
+// Send a search request to Twitter
+var request = http.request({
+
+host: "search.twitter.com",
+port: 80,
+method: "GET",
+     path: "/search.json?q=" + query
+})
+console.log("http req "+ request);
+
+request.addListener("response", function(response) {
+console.log("inside request.addListener");
+        var body = "";
         response.addListener("data", function(data) {
-        	console.log("inside response.addListener");
+         console.log("inside response.addListener");
             body += data;
            // console.log(body);
-        });  
+        });
   
-        response.addListener("end", function() {  
-        	console.log("inside response.addListener end");
+        response.addListener("end", function() {
+         console.log("inside response.addListener end");
             var tweets = JSON.parse(body);
            // console.log(tweets.results.length);
             //console.log(tweets.results[1].from_user);
             //console.log(tweets.from_user);
-           // console.log(tweets.length()); 
-            if(tweets.results.length > 0) {  
-                //tweet_emitter.emit("tweets", tweets); 
+           // console.log(tweets.length());
+            if(tweets.results.length > 0) {
+                //tweet_emitter.emit("tweets", tweets);
                 console.log(tweets.results.length);
-           		console.log(tweets.results[1].from_user);
-           		tweetedUsers(tweets);
-            }  
-        });  
-    });  
+            console.log(tweets.results[1].from_user);
+            tweetedUsers(tweets);
+            }
+        });
+    });
   
-    request.end(); 
+    request.end();
 }
 
 
 
 
 function tweetedUsers(tweets){
-	//var tweetedUsers =[];
-	console.log("in function tweetedUsers");
-	for(var i=1; i< tweets.results.length; i++){
-		//console.log(tweets.results[i].from_user);
-		arrtweetedUsers.push(tweets.results[i].from_user);
-	}
-		
-		
-	console.log(arrtweetedUsers.length+" "+arrtweetedUsers);
-	
+//var tweetedUsers =[];
+console.log("in function tweetedUsers");
+for(var i=1; i< tweets.results.length; i++){
+//console.log(tweets.results[i].from_user);
+arrtweetedUsers.push(tweets.results[i].from_user);
+}
+
+
+console.log(arrtweetedUsers.length+" "+arrtweetedUsers);
+
 }
 
 /*
 function rankUsers(){
-	var rank=[];
-	for(var i=1; i< tweets.results.length; i++){
-		rank.push()
-	}
+var rank=[];
+for(var i=1; i< tweets.results.length; i++){
+rank.push()
+}
 }*/
