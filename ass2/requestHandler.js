51f5286c4dac0f139b3e8e874807f218047735af
soori <charithsoori@gmail.com>
2011-09-25 02:22:33 +0530

    request handler added

---
+++ ass2/requestHandler.js
@@ -0,0 +1,32 @@
+var http = require("http");
+var logic = require("./logic");
+
+var arrtweetedUsers =[];
+var queries = {};
+
+
+
+exports.start=function start(req, res){
+
+    query = req.url.split("?")[1];
+   
+    res.send('Hello World start');
+    console.log(query);
+
+  //  console.log('url =' +url+'  '+'query ='+query);
+
+   // logic.getTweets(query);
+
+    if(!queries[query]) {
+        //setInterval(logic.getTweets, 10000);
+        console.log('....................................');
+        queries[query] = true;  
+    }
+   // setInterval(logic.getTweets, 10000);
+}
+
+
+exports.view =function view(req, res){
+    
+    logic.rankUsers(req, res);
+}
