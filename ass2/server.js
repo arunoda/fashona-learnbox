a643221b5d42be5b041cefc56c9f41764200fa64
soori <charithsoori@gmail.com>
2011-09-25 02:22:52 +0530

    server.js added

---
+++ ass2/server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var requestHandler = require("./requestHandler");
+
+
+var server = express.createServer();
+
+server.get('/',function(req, res){
+
+  requestHandler.start(req, res);
+});
+
+server.get('/view',function(req, res){
+
+  requestHandler.view(req, res);
+});
+
+server.listen(8080);
+console.log('Express server started on port %s', server.address().port);
+
+exports.server=server;
