0c21dcd51b7d96b8f5e0bd8f8313603ee6a6832e
soori <charithsoori@gmail.com>
2011-09-28 16:59:53 +0530

    restler part in haookAction file commented . Work

--- ass3/lib/hookAction.js
+++ ass3/lib/hookAction.js
@@ -4,13 +4,13 @@ exports.send = function send(url){
 	
 	console.log('.....................    kem in     .......................');
 	console.log(url);
-//	console('Restler starting to ping %s',url);
-	
-	rest.get(url)
+	//console('Restler starting to ping %s',url);
+	/*
+	rest.get('http://google.lk')
         .on('complete', function(data) {
         	console.log('notifiation sent');
-        });
-        
+    }); */
+      
 }
 
 
