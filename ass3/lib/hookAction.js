e3f10042d0f3d5436ee2fb48227dd2ff9bba53c7
soori <charithsoori@gmail.com>
2011-09-28 17:01:24 +0530

    working : Things working with twitter and without assembla configuration

--- ass3/lib/hookAction.js
+++ ass3/lib/hookAction.js
@@ -5,11 +5,11 @@ exports.send = function send(url){
 	console.log('.....................    kem in     .......................');
 	console.log(url);
 	//console('Restler starting to ping %s',url);
-	/*
+	
 	rest.get('http://google.lk')
         .on('complete', function(data) {
         	console.log('notifiation sent');
-    }); */
+    }); 
       
 }
 
