7d1551a9580045796960d1f1f9e40ca95373d9e2
soori <charithsoori@gmail.com>
2011-09-28 16:34:00 +0530

    bug fixing

--- ass3/lib/hookAction.js
+++ ass3/lib/hookAction.js
@@ -1,9 +1,11 @@
 var rest=require('restler');
 
 exports.send = function send(url){
+	
 	console.log('.....................    kem in     .......................');
+	console.log(url);
 	console('Restler starting to ping %s',url);
-	rest.get()
+	rest.get(url)
         .on('complete', function(data) {
         	console.log('notifiation sent');
         });
